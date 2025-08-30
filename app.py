24a919c910c9dd82d1947eae4f84b5eb871af5c5
umemura-0678 <yume0678@gmail.com>
2025-08-30 12:04:21 +0900

    変更したよ

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    recieved_message = event.message.text
+    send_message = recieved_message + "さん、こんにちは"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=send_message))
 
 
 if __name__ == "__main__":
