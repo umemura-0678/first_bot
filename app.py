1d21136a1972f8e2f87db2f489e27a41ba1b5a00
umemura-0678 <yume0678@gmail.com>
2025-08-30 11:14:26 +0900

    app.py 修正

--- app.py
+++ app.py
@@ -7,36 +7,41 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-lin_dot_api = LineBotApi(os.getenv(["ACSECC_TOKEN"]))
-handlr = wbHookHandler = os.getenv("ACSECC_TOKEN")
-
 app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
 
 
 @app.route("/")
 def index():
     return "You call index()"
 
+
 @app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    """Messaging APIからの呼び出し関数"""
+    # LINEがリクエストの改ざんを防ぐために付与する署名を取得
+    signature = request.headers["X-Line-Signature"]
+    # リクエストの内容をテキストで取得
     body = request.get_data(as_text=True)
+    # ログに出力
     app.logger.info("Request body: " + body)
+
     try:
-        hanlder.handle(body, signature)
+        # signature と body を比較することで、リクエストがLINEから送信されたものであることを検証
+        handler.handle(body, signature)
     except InvalidSignatureError:
+        # クライアントからのリクエストに誤りがあったことを示すエラーを返す
         abort(400)
-    
-    return 'OK'
-    
+
+    return "OK"
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
-
- 
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
